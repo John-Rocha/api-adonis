d5780823ef25eaa2421043ab98132ed9acdab29f
John-Rocha <52468127+John-Rocha@users.noreply.github.com>
2021-04-07 11:41:47 -0300

    Internacionalization

--- app/Validators/ResetPassoword.js
+++ app/Validators/ResetPassoword.js
@@ -1,11 +1,17 @@
 'use strict'
 
+const Antl = use('Antl')
+
 class ResetPassoword {
   get rules () {
     return {
       token: 'required',
       'password': 'required|confirmed'
     }
+  }
+
+  get messages () {
+    return Antl.list('validation')
   }
 }
 
